c4cd4b0b571b128ec81ec82b6509b45ad7abd12f
Vaibhav Sareen <vaibhavssj3@gmail.com>
2024-06-18 23:24:36 +0530

    Create a subpackage called reusables that contains reusable code snippets that are not crucial but can help produce cleaner code and demonstrations

---
+++ reusables/ui_configs.py
@@ -0,0 +1,7 @@
+import gradio as gr
+
+# Default theme to be used while implementing demos through UI created using gradio
+_DEFAULT_GRADIO_THEME = gr.themes.Soft(primary_hue="teal",
+                                       secondary_hue="purple",
+                                       neutral_hue="cyan",
+                                    )
